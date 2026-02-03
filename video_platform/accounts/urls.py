639f18dbb104a904f3a639ca29a430f99fdc55dc
Mateo mojica <mmojicah.84@gmail.com>
2026-02-03 02:37:59 +0000

    added the urls for the accounts app

---
+++ video_platform/accounts/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from views import RegisterView
+
+app_name = 'accounts'
+
+urlpatterns = [
+  path('register/', RegisterView.as_view(), name = 'register'),
+  path('login/', auth_views.LoginView.as_view(
+    template_name = 'accounts/login.html',
+    redirect_authenticated_user = True
+  ), name = 'login'),
+  path('logout/', auth_views.LogoutView.as_view(
+    template_name = 'accounts/logout.html',
+    redirect_authenticated_user = True
+  ), name = 'logout')
+]
