5053d32bceb5728d2d861411194d95c829453616
Mateo mojica <mmojicah.84@gmail.com>
2026-03-09 21:39:55 -0500

    add cors headers to the project

--- video_platform/video_platform/settings.py
+++ video_platform/video_platform/settings.py
@@ -30,6 +30,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
 
 # Application definition
 
@@ -43,10 +48,12 @@ INSTALLED_APPS = [
     "rest_framework",
     "accounts",
     "videos",
-    "rest_framework.authtoken"
+    "rest_framework.authtoken",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
