9eabce76c304d83086f66f28406e41bf2e041fa0
Mateo mojica <mmojicah.84@gmail.com>
2026-03-10 21:21:04 -0500

    update video serializers to include the video owner information

--- video_platform/videos/serializers.py
+++ video_platform/videos/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from .models import Video
 
+class VideoOwnerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username']
+
 class VideoSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Video
-    fields = '__all__'
+    user = VideoOwnerSerializer(read_only=True)
+
+    class Meta:
+        model = Video
+        fields = '__all__'
 
